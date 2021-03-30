f3808a1b0be9e89a757ad8b4ba7721e84ee61185
Aleksandr <oao@i-cio.ru>
2021-03-30 05:34:10 +0300

    New TableViewConntroller

--- ReadFromFile/TableViewController.swift
+++ ReadFromFile/TableViewController.swift
@@ -12,13 +12,16 @@ class TableViewController: UITableViewController {
     var textArray = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(#line, #function, "Function is load")
             readFromFile()
     }
+    
     func readFromFile (){
         if let  path = Bundle.main.path(forResource: "text", ofType: "txt"){
             if let text = try? String(contentsOfFile: path) {
-                textArray = text.components(separatedBy: "\n\n")
-                print(#line, #function, textArray)
+                textArray = text.components(separatedBy: "\n")
+                print(#line, #function, "Loading file")
+                print(#line, #function, textArray.count)
             }
         }
 
@@ -26,26 +29,28 @@ class TableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        print(#line, #function, textArray.count)
         return textArray.count
-    }
+             }
 
   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
 
+        print(#line, #function, indexPath.row)
         cell.textLabel?.text = "Cell number \(indexPath.row)"
-
+        
         return cell
     }
+
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "detailSegue" {
             if let indexPath = tableView.indexPathForSelectedRow {
